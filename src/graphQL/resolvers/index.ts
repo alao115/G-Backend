19b18e489749a0e6f1b9d5530da4da7dec42ea84
alao115 <chegunmoulero@gmail.com>
2022-01-21 16:53:55 +0100

    on appartment delete resolver fixed v2

--- src/graphQL/resolvers/index.ts
+++ src/graphQL/resolvers/index.ts
@@ -100,10 +100,13 @@ export default {
       if(!data || !appartmentId) throw new UserInputError('Invalid appartment data')
       return appartmentService.update({ id: appartmentId, data})
     },
-    deleteAppartment(parent: any, { appartmentId }: { appartmentId: string }, { appartmentService, publicationService }: { appartmentService: any, publicationService: any }, info: any) {
+    deleteAppartment(parent: any, { appartmentId }: { appartmentId: string }, { appartmentService, publicationService, visitService, reservationService }: { appartmentService: any, publicationService: any, visitService: any, reservationService: any }, info: any) {
       if(!appartmentId) throw new UserInputError('Invalid appartment data')
 
-      return publicationService.deleteMany({ appartment: appartmentId }).then(() => appartmentService.delete({ id: appartmentId}))
+      return publicationService.deleteMany({ appartment: appartmentId })
+              .then(() => reservationService.deleteMany({ appartment: appartmentId }))
+              .then(() => visitService.deleteMany({ appartment: appartmentId }))
+              .then(() => appartmentService.delete({ id: appartmentId}))
     },
 
     createAppartmentType(parent: any, { data }: { data: any}, { appartmentTypeService }: { appartmentTypeService: any }, info: any) {
