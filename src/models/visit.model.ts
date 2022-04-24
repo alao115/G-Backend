f263d3982723869bc006a5df408012195150bce5
alao115 <chegunmoulero@gmail.com>
2022-04-24 18:17:01 +0100

    latest updates

--- src/models/visit.model.ts
+++ src/models/visit.model.ts
@@ -34,7 +34,7 @@ const visitSchema = new Schema<Visit, Model<Visit>, Visit>({
   date: {
     type: String,
     required: true,
-    get: (v: any ) => moment.unix(v).format("YYYY-MM-DD")
+    get: (v: any ) => moment.unix(v).format("DD-MM-YYYY")
   },
   status: {
     type: String,
