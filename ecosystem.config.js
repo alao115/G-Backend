ef54e84f4ac13910d08caa3fecc5519f19e512b7
alao115 <chegunmoulero@gmail.com>
2022-05-17 12:43:28 +0100

    pm2 config updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,19 @@ module.exports = {
     {
       name: 'gontche_backend_prod',
       script: 'npm run start',
-      watch: true
+      watch: true,
+      env: {
+        APP_NAME: "API Gontche",
+        APP_PORT: 7500,
+        APP_ENV: 'development',
+        DB_NAME: 'mongodb://localhost/gontche',
+        FRONTEND_URL: 'https://gontche.com',
+        EMAIL_HOST: 'mail.privateemail.com',
+        EMAIL_USER: 'catch-all@rcg.studio',
+        EMAIL_PASSWORD: 'Bbb123@rcg',
+        EMAIL_SECURE: true,
+        EMAIL_PORT: 465
+      }
     },
     {
       name: 'gontche_backend_dev',
