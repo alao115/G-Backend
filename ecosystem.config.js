722e62c239723b423e32fbcda7c58e2db758b5b3
alao115 <chegunmoulero@gmail.com>
2022-05-30 08:25:35 +0100

    env variables removed from pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,16 +5,6 @@ module.exports = {
       script: 'npm run start',
       watch: true,
       env: {
-        APP_NAME: "API Gontche",
-        APP_PORT: 7500,
-        APP_ENV: 'development',
-        DB_NAME: 'mongodb://gontche_prod:Gontche!atDBProd@localhost:27017/gontche',
-        FRONTEND_URL: 'https://gontche.com',
-        EMAIL_HOST: 'mail.privateemail.com',
-        EMAIL_USER: 'catch-all@rcg.studio',
-        EMAIL_PASSWORD: 'Bbb123@rcg',
-        EMAIL_SECURE: true,
-        EMAIL_PORT: 465
       }
     },
     {
